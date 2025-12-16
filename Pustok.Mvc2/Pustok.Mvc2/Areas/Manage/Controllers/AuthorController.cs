63aa12777ceca4972ada0dbb456d24084c1935f8
emilfmi.lang <emilfmi@code.edu.az>
2025-12-16 10:38:35 +0400

    added

---
+++ Pustok.Mvc2/Pustok.Mvc2/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Mvc2.Data;
+
+namespace Pustok.Mvc2.Areas.Manage.Controllers;
+[Area("Manage")]
+
+public class AuthorController(AppDbContext appDbContext) : Controller
+{
+    public IActionResult Index()
+    {
+
+        var authors = appDbContext.Authors.ToList();
+        return View(authors);
+    }
+}
---
+++ Pustok.Mvc2/Pustok.Mvc2/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.Mvc2.Data;
+
+namespace Pustok.Mvc2.Areas.Manage.Controllers;
+[Area("Manage")]
+
+
+public class SliderController(AppDbContext appDbContext) : Controller
+{
+    public IActionResult Index()
+    {
+        var sliders = appDbContext.Sliders.ToList();
+        return View(sliders);
+    }
+}
